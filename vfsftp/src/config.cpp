363c680b3f1ce63f6744d2f536d8449b1240ba14
Andrey Borovsky <aborovskiy@adalisk.com>
2021-07-27 12:31:15 +0300

    Update config.cpp and config.h

---
+++ vfsftp/src/config.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andrey on 7/27/21.
+//
+
+#include "config.h"
---
+++ vfsftp/src/config.h
@@ -0,0 +1,14 @@
+//
+// Created by andrey on 7/27/21.
+//
+
+#ifndef FINEFTP_CONFIG_H
+#define FINEFTP_CONFIG_H
+
+
+class Config {
+
+};
+
+
+#endif //FINEFTP_CONFIG_H
